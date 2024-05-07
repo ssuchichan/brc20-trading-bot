455330f02561a91116488914d929bccb5fee86a4
ssuchichan <ssuchichan@gmail.com>
2024-05-07 20:01:07 +0800

    fix

--- db/db.go
+++ db/db.go
@@ -13,17 +13,36 @@ var master struct {
 	dbx atomic.Value
 }
 
+var remoteMaster struct{
+	dbx atomic.Value
+}
+
 func Master() *sqlx.DB {
 	return master.dbx.Load().(*sqlx.DB)
 }
 
+func RemoteMaster() *sqlx.DB {
+	return remoteMaster.dbx.Load().(*sqlx.DB)
+}
+
 func init() {
-	var dbx *sqlx.DB
-	var err error
+	var (
+		dbx *sqlx.DB
+		remoteDbx *sqlx.DB
+		err error
+	)
 	dbURL := os.Getenv("DATABASE_URL")
 	dbx, err = sqlx.Connect("postgres", dbURL)
 	if err != nil {
 		panic(err)
 	}
 	master.dbx.Store(dbx)
+
+
+	dbURLRemote := os.Getenv("REMOTE_DATABASE_URL")
+	remoteDbx, err = sqlx.Connect("postgres", dbURLRemote)
+	if err != nil {
+		panic(err)
+	}
+	remoteMaster.dbx.Store(remoteDbx)
 }
